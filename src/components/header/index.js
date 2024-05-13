3a2c35a9031a05f1cf515cfb85dae312b101a7cd
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-13 15:34:00 -0300

    Adicionando botão de pesquisa no header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Container } from './styles'
+import SearchButton from '../buttonsearch'
 
 export default function Header() {
   return (
     <Container>
-        Header
+        <SearchButton />
     </Container>
   )
 }
