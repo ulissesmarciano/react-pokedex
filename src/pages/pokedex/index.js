8e90b62508001a0445655b6ad6b60fcf80b131a6
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-15 16:31:21 -0300

    Modificação da página adicionando os componentes input e titulo

--- src/pages/pokedex/index.js
+++ src/pages/pokedex/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import Header from '../../components/header'
 
-import { Container, PokemonCardContainer } from './styles'
+import { Container, PokemonCardContainer, TitleContainer } from './styles'
+
+import Header from '../../components/header'
 import PokemonCard from '../../components/pokemon-card'
+import SearchInput from '../../components/search-input'
 
 export default function Pokedex() {
   return (
     <Container>
       <Header />
-        <PokemonCardContainer>
+        <div>
+          <TitleContainer>
+            <h2>Pokédex</h2>
+            <p>Procure por seu pokémon pelo nome ou seu número</p>
+          </TitleContainer>
+          <div>
+            <SearchInput />
+          </div>
+        </div>
+        {/* <PokemonCardContainer>
           <PokemonCard />
           <PokemonCard />
           <PokemonCard />
@@ -29,7 +40,7 @@ export default function Pokedex() {
           <PokemonCard />
           <PokemonCard />
           <PokemonCard />
-        </PokemonCardContainer>
+        </PokemonCardContainer> */}
     </Container>
   )
 }
