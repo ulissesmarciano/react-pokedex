cffa4b70135769b38a0fad487d6d68fa01a73624
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-15 16:32:43 -0300

    Aplicando a folha de estilos e regulando o título na página pokédex

--- src/pages/pokedex/styles.js
+++ src/pages/pokedex/styles.js
@@ -1,17 +1,33 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    background-color: yellow;
+export const Container = styled.div``
 
-    
+export const TitleContainer = styled.div`
+    margin: .8rem 1.2rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+
+    h2 {
+        margin-bottom: .3rem;
+    }
+
+    p{
+        color: #b44b5095;
+        font-weight: 500;
+    }
 `
 
 export const PokemonCardContainer = styled.ul`
-    max-width: 1500px;
-    margin: auto;
+    margin: 1rem;
+    padding: 1rem;
+    border: 2px solid #b44b50;
+    border-radius: 10px;
 
     display: grid;
-    grid-template-columns: 1fr ;
+    grid-template-columns: 1fr;
     gap: .5rem;
 
 `
