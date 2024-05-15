284067c7808eca1d4beef04349efdac3ba115591
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-15 16:34:20 -0300

    Adicionando a fonte Roboto ao estilo global

--- src/style/global.js
+++ src/style/global.js
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: Roboto;
         list-style: none;
     }
 `
