2b1ad847d023f3d82a94325e267bd47ff25edfea
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-18 22:53:18 -0300

    Adicionando header com back arrow

--- src/pages/pokemon/index.js
+++ src/pages/pokemon/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, BackArrowContainer } from './styles'
 import { Link } from 'react-router-dom'
+
+import BackArrow from '../../assets/icons/left-arrow-icon.svg'
 
 export default function Pokemon() {
   return (
     <Container>
-        <Link to="/" >Voltar</Link>
-        Pokemon
+        <BackArrowContainer>
+          <Link to="/" ><img src={BackArrow} alt='ícone voltar a página anterior'/> </Link>
+        </BackArrowContainer>
     </Container>
   )
 }
--- src/pages/pokemon/styles.js
+++ src/pages/pokemon/styles.js
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+    background-color: red;
+`
+
+export const BackArrowContainer = styled.div`
+    background-color: blue;
+    img {
+        height: 1.4rem;
+    }
+`
