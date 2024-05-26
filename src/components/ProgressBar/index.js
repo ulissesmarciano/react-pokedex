0ca770c13f4397a0575039e6b362dd72411c9b8b
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-25 21:31:39 -0300

    Criação da barra de progresso

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ProgressBarBackground, ProgressPowerBar } from './styles'
+
+
+
+export default function ProgressBar({progress}) {
+  return (
+        <ProgressBarBackground>
+          <ProgressPowerBar />
+          {/* <ProgressPowerBar style={{width:`${progress}%`, background: `${progress < 50 ? "red" : "green"}`}}/> */}
+        </ProgressBarBackground>
+  )
+}
---
+++ src/components/ProgressBar/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ProgressBarBackground = styled.div`
+    margin-left: 1rem;
+    height: 3px;
+    width: 100%;
+
+    background-color: #c1c1c1;
+`
+
+export const ProgressPowerBar = styled.div`
+    height: 3px;
+    width: 50px;
+
+    background-color: green;
+`
+
