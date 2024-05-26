d280c6f4060e3c447739c7b16354b0c6b4c16f0d
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-25 21:35:38 -0300

    Criação da folha de estilos para stats e versions tab screen

---
+++ src/components/stats-info-tab-screen/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StatsTabScreenSection = styled.ul`
+
+    li {
+        margin-bottom: 1rem;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        color: #c1c1c1;
+        font-weight: 700;
+
+        p {
+            color: #000;
+            font-weight: 500;
+        }
+
+        span {
+            width: 78%;
+
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+
+            color: #000;
+            font-weight: 500;
+
+        }
+    }
+`
