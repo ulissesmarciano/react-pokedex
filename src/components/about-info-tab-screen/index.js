75f7798c3d5c4309513c36a6b1b72be41d98892c
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-26 17:43:02 -0300

    Adicionando informações com em mockup e folhas de estilo na about-info-tab-screen

--- src/components/about-info-tab-screen/index.js
+++ src/components/about-info-tab-screen/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Container} from './styles'
+import { Container, StoryContainer, WeaknessesContainer} from './styles'
 
 
 export default function AboutInfoTabScreen() {
   return (
     <Container>
-      
+      <StoryContainer
+      >
+        <h5>Story</h5>
+        <p>From the time it is born, a flame burns at the tip its tail, its lifi would end if  the flame were to go out. It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.</p>
+      </StoryContainer
+      >
+      <WeaknessesContainer>
+        <h5>Weaknesses</h5>
+        <ul>
+          <li>Fire</li>
+          <li>Water</li>
+          <li>Ground</li>
+        </ul>
+      </WeaknessesContainer>
     </Container>
   )
 }
--- src/components/about-info-tab-screen/styles.js
+++ src/components/about-info-tab-screen/styles.js
@@ -1,9 +1,29 @@
 import styled from "styled-components";
 
-export const Container = styled.ul`
-    
-        
-    
+export const Container = styled.section`
+    margin: 1rem 1rem;
+    h5 {
+        margin-bottom: .8rem;
+        font-size: .8rem;
+    }
 `
 
+export const StoryContainer = styled.div`
 
+    p {
+        margin-bottom: 1rem;
+    }
+`
+export const WeaknessesContainer = styled.div`
+    ul {
+        display: flex;
+
+        li {
+            margin-left: .5rem;
+            padding: 0 .8rem;
+
+            background-color: blueviolet;
+            border-radius: 50px;
+        }
+    }
+`
