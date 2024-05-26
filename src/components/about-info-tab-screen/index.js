473635a5c485df3a33483a81fdfc5b603a991fd8
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-26 15:58:22 -0300

    Removendo o conteúdo e por correção enviando para outro componente

--- src/components/about-info-tab-screen/index.js
+++ src/components/about-info-tab-screen/index.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { Container, DataItemAboutList } from './styles'
+import { Container} from './styles'
 
-import MaleSimbol from '../../assets/icons/male.png'
-import FemaleSimbol from '../../assets/icons/female.png'
 
 export default function AboutInfoTabScreen() {
   return (
     <Container>
-      <DataItemAboutList>Height<span>2.30 Feet (0.70)</span></DataItemAboutList>
-      <DataItemAboutList>Weight<span>15.6Lbs (6.9kg)</span></DataItemAboutList>
-      <DataItemAboutList>Abilities<span>Overgrow, Chlorphyll</span></DataItemAboutList>
-
-      <h5>Breeding</h5>
-      <DataItemAboutList>Gender<span><img src={MaleSimbol} alt='Simbolo Macho '/> 87,5%</span><span><img src={FemaleSimbol} alt='Simbolo Fêmea'/> 12,5%</span></DataItemAboutList>
-      <DataItemAboutList>Egg Group<span>Monster</span></DataItemAboutList>
-      <DataItemAboutList>Egg Cycle<span>Plant</span></DataItemAboutList>
+      
     </Container>
   )
 }
