a44951650ea9155d9fbf49d3cd6924ddd59bfc77
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-05-30 21:53:47 -0300

    Adicinando a primeira requisição não remota

--- src/pages/pokedex/index.js
+++ src/pages/pokedex/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
+import axios from 'axios'
 
 import { Container, PokemonCardContainer, TitleContainer } from './styles'
 
@@ -6,10 +8,33 @@ import PokedexHeader from '../../components/pokedex-header'
 import PokemonCard from '../../components/pokemon-card'
 import SearchBar from '../../components/search-bar'
 
+
 export default function Pokedex() {
+  
+  useEffect(() => {
+    getPokemons();
+  }, [])
+
+  const [pokemons, setPokemons] = useState([])
+
+  const getPokemons = () => {
+    var limit = 151
+    var endpoints = []
+
+    for (var i = 1; i <= limit; i++){
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    } 
+
+    axios
+    .all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res)).catch((err) => console.log(err))
+  }
+
+
+  
   return (
     <Container>
       <PokedexHeader />
+      {console.log(pokemons)}
         <div>
           <TitleContainer>
             <h2>Pokédex</h2>
