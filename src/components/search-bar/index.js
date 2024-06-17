b342702cbd02dbc783e1934ccc885563a8907a6c
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-06-16 23:10:24 -0300

    Passando parâmetros para o componente renderizado na página

--- src/components/search-bar/index.js
+++ src/components/search-bar/index.js
@@ -4,12 +4,12 @@ import { Container, SearchButton, SearchLabel } from './styles'
 
 import SearchIcon from '../../assets/icons/search-icon.svg' 
 
-export default function SearchBar() {
+export default function SearchBar({onChange, value}) {
   return (
     <Container>
       <SearchLabel>
         <img src={SearchIcon} alt='Ícone de pesquisa' />
-        <input placeholder='Nome ou Número' type='text'/>
+        <input placeholder='Nome ou Número' type='text' onChange={onChange} value={value}/>
       </SearchLabel>
         <SearchButton>Buscar</SearchButton>  
     </Container>
