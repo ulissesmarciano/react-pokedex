58ecadc0156682b1b85a15472bcf7000b07e19d3
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-06-17 16:55:48 -0300

    Aplicando folha de estilos na seção type do pokémon renderizado

--- src/components/evolution-item-list/styles.js
+++ src/components/evolution-item-list/styles.js
@@ -159,6 +159,80 @@ export const PokemonEvolutionItem = styled.ul`
                 
                 display: flex;
                 text-align: center;
+
+                &.normal {
+                     background-color: #a6a87790;
+                }
+
+                &.grass {
+                    background-color: #77c850;
+                }
+
+                &.fire {
+                    background-color: #ee7f30;
+                }
+
+                &.water {
+                    background-color: #678fee;
+                }
+
+                &.electric {
+                    background-color: #f7cf2e;
+                }
+
+                &.ice {
+                    background-color: #98d5d7;
+                }
+
+                &.ground {
+                    background-color: #dfbf69;
+                }
+
+                &.flying {
+                    background-color: #a98ff0;
+                }
+
+                &.poison {
+                    background-color: #a040a0;
+                    color: #fff;
+                }
+
+                &.fighting {
+                    background-color: #bf3029;
+                }
+
+                &.psychic {
+                    background-color: #f65687;
+                }
+
+                &.dark {
+                    background-color: #725847;
+                }
+
+                &.rock {
+                    background-color: #b8a137;
+                }
+
+                &.bug {
+                    background-color: #a8b720;
+                }
+
+                &.ghost {
+                    background-color: #6e5896;
+                }
+                
+                &.steel {
+                    background-color: #b9b7cf;
+                }
+
+                &.dragon {
+                    background-color: #6f38f6;
+                }
+
+                &.fairy {
+                    background-color: #f9aec7;
+                }
+        
             }
 
             .name-and-title-section div {
