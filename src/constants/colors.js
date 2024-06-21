fbf17cdb65744bbbf9a8fd23cced8e4d3a06c378
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-06-20 22:01:17 -0300

    Criação da pasta constants para movimentação, por convenção dos elementos de folha de cores que está componetizado

---
+++ src/constants/colors.js
@@ -0,0 +1,167 @@
+import { css } from "styled-components";
+
+export const pokemonTypeStyles = css`
+    &.normal {
+        background-color: #a6a877;
+    }
+
+    &.grass {
+        background-color: #77c850;
+    }
+
+    &.fire {
+        background-color: #ee7f30;
+    }
+
+    &.water {
+        background-color: #678fee;
+    }
+
+    &.electric {
+        background-color: #f7cf2e;
+    }
+
+    &.ice {
+        background-color: #98d5d7;
+    }
+
+    &.ground {
+        background-color: #dfbf69;
+    }
+
+    &.flying {
+        background-color: #a98ff0;
+    }
+
+    &.poison {
+        background-color: #a040a0;
+    }
+
+    &.fighting {
+        background-color: #bf3029;
+    }
+
+    &.psychic {
+        background-color: #f65687;
+    }
+
+    &.dark {
+        background-color: #725847;
+    }
+
+    &.rock {
+        background-color: #b8a137;
+    }
+
+    &.bug {
+        background-color: #a8b720;
+    }
+
+    &.ghost {
+        background-color: #6e5896;
+    }
+    
+    &.steel {
+        background-color: #b9b7cf;
+    }
+
+    &.dragon {
+        background-color: #6f38f6;
+    }
+
+    &.fairy {
+        background-color: #f9aec7;
+    }
+`
+
+export const pokemonEvolutionStyles = css`
+    &.normal {
+        background-color: #a6a87710;
+        border: 2px solid #a6a877;
+    }
+
+    &.grass {
+        background-color: #77c85010;
+        border: 2px solid #77c850;
+    }
+
+    &.fire {
+        background-color: #ee7f3010;
+        border: 2px solid #ee7f30;
+    }
+
+    &.water {
+        background-color: #678fee10;
+        border: 2px solid #678fee;
+    }
+
+    &.electric {
+        background-color: #f7cf2e10;
+        border: 2px solid #f7cf2e;
+    }
+
+    &.ice {
+        background-color: #98d5d710;
+        border: 2px solid #98d5d7;
+    }
+
+    &.ground {
+        background-color: #dfbf6910;
+        border: 2px solid #dfbf69;
+    }
+
+    &.flying {
+        background-color: #a98ff010;
+        border: 2px solid #a98ff0;
+    }
+
+    &.poison {
+        background-color: #a040a010;
+        border: 2px solid #a040a0;
+    }
+
+    &.fighting {
+        background-color: #bf302910;
+        border: 2px solid #bf3029;
+    }
+
+    &.psychic {
+        background-color: #f6568710;
+        border: 2px solid #f65687;
+    }
+
+    &.dark {
+        background-color: #72584710;
+        border: 2px solid #725847;
+    }
+
+    &.rock {
+        background-color: #b8a13710;
+        border: 2px solid #b8a137;
+    }
+
+    &.bug {
+        background-color: #a8b72010;
+        border: 2px solid #a8b720;
+    }
+
+    &.ghost {
+        background-color: #6e589610;
+        border: 2px solid #6e5896;
+    }
+    
+    &.steel {
+        background-color: #b9b7cf10;
+        border: 2px solid #b9b7cf;
+    }
+
+    &.dragon {
+        background-color: #6f38f610;
+        border: 2px solid #6f38f6;
+    }
+
+    &.fairy {
+        background-color: #f9aec710;
+        border: 2px solid #f9aec7;
+    }
+`
