514059755cb6631eb0aa8b19d2e7c2903a07bbd9
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-06-20 22:04:46 -0300

    Importação dos hooks

--- src/pages/pokemon/index.js
+++ src/pages/pokemon/index.js
@@ -1,118 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import api from '../../services/api';
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { usePokemonLimit } from '../../contexts/PokemonLimitContext';
-import typeWeaknesses from '../../constants/typeWeaknesses';
-
-import { Container, EvolutionContainer, EvolutionImageContainer, InfoTabListContainer, LeftSideSection, NumberPageContainer, PokemonImageContainer, PokemonNameContainer, RightSideSection, SkillContainer } from './styles';
-
+import useFetchPokemonData from '../../hooks/useFetchPokemonData';
+import useFetchPokemonList from '../../hooks/useFetchPokemonList';
+import useFetchEvolutionData from '../../hooks/useFetchEvolutionData';
 import PokemonHeader from '../../components/pokemon-header';
-import InfoTabList from '../../components/info-tab-list';
-import EvolutionItemList from '../../components/evolution-item-list';
 import PokemonPageLoader from '../../components/pokemon-page-loader';
 import PokemonTypesItem from '../../components/pokemon-types-item';
-
 import PokebalImage from '../../assets/icons/pokeball-icon.svg';
+import InfoTabList from '../../components/info-tab-list'
+import {
+  Container,
+  EvolutionContainer,
+  EvolutionImageContainer,
+  InfoTabListContainer,
+  LeftSideSection,
+  NumberPageContainer,
+  PokemonImageContainer,
+  PokemonNameContainer,
+  RightSideSection,
+  SkillContainer
+} from './styles';
+import EvolutionItemList from '../../components/evolution-item-list';
 
-export default function Pokemon() {
-  const [pokemonData, setPokemonData] = useState(null);
-  const [evolutionDataList, setEvolutionDataList] = useState([]);
-  const [pokemonList, setPokemonList] = useState([]);
+const typeWeaknesses = {
+  normal: ['fighting'],
+  water: ['electric', 'grass'],
+  grass: ['fire', 'ice', 'flying', 'poison', 'bug'],
+  fire: ['water', 'ground', 'rock'],
+  fighting: ['flying', 'psychic', 'fairy'],
+  psychic: ['ghost', 'dark', 'bug'],
+  ghost: ['ghost', 'dark'],
+  dark: ['fighting', 'fairy', 'bug'],
+  fairy: ['steel', 'poison'],
+  flying: ['electric', 'rock', 'ice'],
+  bug: ['fire', 'flying'],
+  rock: ['fighting', 'water', 'grass', 'ground', 'steel'],
+  ground: ['water', 'grass', 'ice'],
+  ice: ['fighting', 'fire', 'rock', 'steel'],
+  steel: ['fire', 'fighting', 'ground'],
+  poison: ['psychic', 'ground'],
+  electric: ['ground'],
+  dragon: ['dragon', 'fairy', 'ice']
+};
+
+const Pokemon = () => {
   const { name } = useParams();
   const { limit } = usePokemonLimit();
-
-  const fetchPokemonData = async () => {
-    try {
-      const response = await api.get(`/pokemon/${name}`);
-      const pokemon = response.data;
-
-      const speciesResponse = await api.get(pokemon.species.url);
-      const speciesData = speciesResponse.data;
-
-      const evolutionChainResponse = await api.get(speciesData.evolution_chain.url);
-      const evolutionChainData = evolutionChainResponse.data;
-
-      const typesData = await Promise.all(pokemon.types.map(async (type) => {
-        const typeResponse = await api.get(type.type.url);
-        return typeResponse.data;
-      }));
-
-      setPokemonData({
-        ...pokemon,
-        species: speciesData,
-        evolution_chain: evolutionChainData,
-        types: typesData,
-      });
-
-      fetchEvolutionData(evolutionChainData.chain);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  const fetchPokemonList = async () => {
-    try {
-      const response = await api.get(`/pokemon?limit=${limit}`);
-      setPokemonList(response.data.results);
-    } catch (error) {
-      console.error('Error fetching Pokémon list:', error);
-    }
-  };
-
-  const fetchEvolutionData = async (chain) => {
-    const fetchImage = async (name) => {
-      try {
-        const response = await api.get(`/pokemon/${name}`);
-        return response.data.sprites.front_default;
-      } catch (error) {
-        console.error('Error fetching evolution image:', error);
-        return null;
-      }
-    };
-
-    const fetchPokemonTypes = async (name) => {
-      try {
-        const response = await api.get(`/pokemon/${name}`);
-        return response.data.types.map(type => type.type.name);
-      } catch (error) {
-        console.error('Error fetching Pokémon types:', error);
-        return null;
-      }
-    };
-
-    const evolutionDataList = [];
-
-    const fetchChainData = async (chain) => {
-      const image = await fetchImage(chain.species.name);
-      const types = await fetchPokemonTypes(chain.species.name);
-      evolutionDataList.push({ Name: chain.species.name, Types: types, Photo: image });
-
-      if (chain.evolves_to.length > 0) {
-        for (const evo of chain.evolves_to) {
-          const evoImage = await fetchImage(evo.species.name);
-          const evoTypes = await fetchPokemonTypes(evo.species.name);
-          evolutionDataList.push({ Name: evo.species.name, Types: evoTypes, Photo: evoImage });
-
-          if (evo.evolves_to.length > 0) {
-            for (const subEvo of evo.evolves_to) {
-              const subEvoImage = await fetchImage(subEvo.species.name);
-              const subEvoTypes = await fetchPokemonTypes(subEvo.species.name);
-              evolutionDataList.push({ Name: subEvo.species.name, Types: subEvoTypes, Photo: subEvoImage });
-            }
-          }
-        }
-      }
-    };
-
-    await fetchChainData(chain);
-    setEvolutionDataList(evolutionDataList);
-  };
-
-  useEffect(() => {
-    fetchPokemonData();
-    fetchPokemonList();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [name]);
+  const pokemonData = useFetchPokemonData(name);
+  const pokemonList = useFetchPokemonList(limit);
+  const evolutionDataList = useFetchEvolutionData(pokemonData?.species?.evolution_chain?.url);
 
   if (!pokemonData || pokemonList.length === 0) {
     return (
@@ -137,9 +74,6 @@ export default function Pokemon() {
     return pokemonList[prevIndex].name;
   };
 
-  const nextPokemonName = getNextPokemonName();
-  const prevPokemonName = getPrevPokemonName();
-
   const calculateWeaknesses = (types) => {
     const weaknesses = new Set();
     types.forEach((type) => {
@@ -150,15 +84,13 @@ export default function Pokemon() {
     return Array.from(weaknesses);
   };
 
-  const weaknesses = calculateWeaknesses(pokemonData.types.map((type) => type.name));
+  const weaknesses = calculateWeaknesses(pokemonData.types.map(type => type.name));
 
   return (
     <Container>
       <PokemonHeader />
       <NumberPageContainer>
-        <Link 
-          to={`/pokemon/${prevPokemonName}`}
-        >
+        <Link to={`/pokemon/${getPrevPokemonName()}`}>
           #{(pokemonData.id < 10 ? `0${pokemonData.id - 1}` : `${pokemonData.id - 1}`)}
         </Link>
       </NumberPageContainer>
@@ -178,10 +110,17 @@ export default function Pokemon() {
             ))}
           </ul>
         </SkillContainer>
-        <PokemonImageContainer className={pokemonData.types[0].name} >
-          <img className='pokemon-image'  src={pokemonData.sprites.other.dream_world.front_default === null ? pokemonData.sprites.other['official-artwork'].front_default : pokemonData.sprites.other.dream_world.front_default}
-            alt={`Foto do pokémon ${pokemonData.name}`} />
-          <img className='pokeball-image' src={PokebalImage} alt='animação de pokébola girando atrás da foto'/>
+        <PokemonImageContainer className={pokemonData.types[0].name}>
+          <img
+            className='pokemon-image'
+            src={pokemonData.sprites.other.dream_world.front_default === null ? pokemonData.sprites.other['official-artwork'].front_default : pokemonData.sprites.other.dream_world.front_default}
+            alt={`Foto do pokémon ${pokemonData.name}`}
+          />
+          <img
+            className='pokeball-image'
+            src={PokebalImage}
+            alt='animação de pokébola girando atrás da foto'
+          />
         </PokemonImageContainer>
         <EvolutionContainer>
           <h6>Evolutions</h6>
@@ -189,18 +128,18 @@ export default function Pokemon() {
             {evolutionDataList.map((evolution, index) => (
               <EvolutionItemList 
                 key={index}
+                to={`/pokemon/${evolution.Name}`}
                 src={evolution.Photo}
                 alt={`foto da evolução ${evolution.Name}`}
                 className={evolution.Types[0]}
                 pokemonName={evolution.Name}
-                type={evolution.Types.map((name, index) => 
-                  <PokemonTypesItem
-                    key={index} 
+                type={evolution.Types.map((name, index) => (
+                  <PokemonTypesItem 
+                    key={index}
                     typeBackground={name}
                     typeName={name}
-                  />
-                )}
-                to={`/pokemon/${evolution.Name}`}
+                    />
+                ))}
               />
             ))}
           </EvolutionImageContainer>
@@ -209,42 +148,39 @@ export default function Pokemon() {
       <RightSideSection>
         <InfoTabListContainer>
           <InfoTabList
-            //attributes
-            weakness={
-              weaknesses
-              .map((weakness, index) => 
-              <PokemonTypesItem 
+            weakness={weaknesses.map((weakness, index) => (
+              <PokemonTypesItem
+                key={index}
                 typeBackground={weakness}
                 typeName={weakness}
-                key={index}
               />
-            )}
+            ))}
             height={`${(pokemonData.height / 3.048).toFixed(2)} feet (${(pokemonData.height / 10).toFixed(2)} cm)`}
             weight={`${(pokemonData.weight / 4.436).toFixed(2)} lbs (${(pokemonData.weight / 10).toFixed(1)} kg)`}
-            abilities={pokemonData.abilities.map((ability) => ability.ability.name).reduce((prev, curr) => [prev, ', ', curr])}
-            //breeding
+            abilities={pokemonData.abilities.map((ability) => ability.ability.name).join(', ')}
             malePercentage={`${((pokemonData.species.gender_rate * -100) / 8) + 100}%`}
             femalePercentage={`${((pokemonData.species.gender_rate * 100) / 8)}%`}
             eggGroup={pokemonData.species.egg_groups[0]?.name || ''}
             eggCycle={pokemonData.species.egg_groups[1]?.name || 'Dont Have'}
-            //stats
             hp={pokemonData.stats[0].base_stat}
             attack={pokemonData.stats[1].base_stat}
             defense={pokemonData.stats[2].base_stat}
             spAtk={pokemonData.stats[3].base_stat}
             spDef={pokemonData.stats[4].base_stat}
             speed={pokemonData.stats[5].base_stat}
-            total={pokemonData.stats.map((base) => (base.base_stat)).reduce((prev, curr) => prev + curr)}
+            total={pokemonData.stats.map((base) => base.base_stat).reduce((prev, curr) => prev + curr)}
           />
         </InfoTabListContainer>
       </RightSideSection>
       <NumberPageContainer>
-        {nextPokemonName && (
-          <Link to={`/pokemon/${nextPokemonName}`}>
+        {getNextPokemonName() && (
+          <Link to={`/pokemon/${getNextPokemonName()}`}>
             #{pokemonData.id < 10 ? `0${pokemonData.id + 1}` : `${pokemonData.id + 1}`}
           </Link>
         )}
       </NumberPageContainer>
     </Container>
   );
-}
+};
+
+export default Pokemon;
--- src/pages/pokemon/styles.js
+++ src/pages/pokemon/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { pokemonTypeStyles } from "../../style/pokemon-types-style";
+import { pokemonTypeStyles } from "../../constants/colors";
 
 export const Container = styled.div`
     margin: 4rem .5rem 0;
