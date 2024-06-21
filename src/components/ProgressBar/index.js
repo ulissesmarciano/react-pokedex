46f3d2c609605d6e1c4402c5f9882472f2dabc18
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-06-20 22:49:40 -0300

    Atualização da parametrização das propriedades do componente para estilos

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { ProgressBarBackground, ProgressPowerBar } from './styles'
+import React from 'react';
+import { ProgressBarBackground, ProgressPowerBar } from './styles';
 
+const ProgressBar = ({ progress }) => {
+  // Lógica para determinar a largura e a cor da barra de progresso
+  const width = `${progress}%`;
+  const color = progress < 50 ? "red" : "green";
 
-
-export default function ProgressBar({progress}) {
   return (
-        <ProgressBarBackground>
-          <ProgressPowerBar style={{width:`${progress}%`, background: `${progress < 50 ? "red" : "green"}`}}/>
-        </ProgressBarBackground>
-  )
-}
+    <ProgressBarBackground>
+      <ProgressPowerBar width={width} color={color} />
+    </ProgressBarBackground>
+  );
+};
+
+export default ProgressBar;
+
--- src/components/ProgressBar/styles.js
+++ src/components/ProgressBar/styles.js
@@ -1,18 +1,22 @@
-import styled from "styled-components";
+// styles.js
+
+import styled from 'styled-components';
 
 export const ProgressBarBackground = styled.div`
     margin-left: 1rem;
-    height: 5px;
-    width: 100%;
+  min-width: 100%;
+  height: 5px;
 
-    background-color: #c1c1c1;
-    border-radius: 2px;
-`
+  background-color: lightgray;
+  border-radius: 10px;
+`;
 
 export const ProgressPowerBar = styled.div`
-    height: 5px;
-    width: 50px;
-
-    border-radius: 2px;
-`
+  height: 100%;
+  border-radius: 8px;
+  /* Estilo específico do ProgressPowerBar */
+  width: ${({ width }) => width}; /* Utilizando a largura passada como propriedade */
+  max-width: 100%; /* Garante que a largura máxima seja 100% do container */
+  background-color: ${({ color }) => color}; /* Cor da barra de progresso */
+`;
 
