a84050168f94b4cd823253f7ee30bc3f6353ebad
Ulisses <ulissesmkt2104@gmail.com>
2024-10-16 23:06:11 -0300

    Criação: pasta de objetos para organizar os status do  pokemon.

---
+++ src/objects/pokemon.js
@@ -0,0 +1,16 @@
+const pokemon = {
+    name: '',
+    types: [],
+    story: '',
+    picture: '',
+
+    weaknesses: [],
+    evolution: [],
+    height: '',
+    weight: '',
+    abilities: [],
+    genderRate: '',
+    eggGroup: '',
+    eggCycle: '',
+    stats: []
+}
