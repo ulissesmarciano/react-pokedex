706a73ed8d3f1e27f89dbfb260499768491b2e5a
Ulisses <ulissesmkt2104@gmail.com>
2024-11-05 23:06:09 -0300

    Atualiação: Adicionando novos atributos para o objeto

--- src/hooks/usePokemonData.js
+++ src/hooks/usePokemonData.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 import useFetchPokemonData from './useFetchPokemonData'
 import useFetchEvolutionData from './useFetchEvolutionData';
+import { getPokemonImage } from '../utils/pokemonUtils';
 
 const usePokemonData = (name) => {
     const [pokemon, setPokemon] = useState({
         name: '',
         id: '',
+        types: [],
+        background: '',
+        picture: '',
     });
 
     const pokemonData = useFetchPokemonData(name);
@@ -17,9 +21,27 @@ const usePokemonData = (name) => {
                 ...prevState,
                 name: pokemonData.name,
                 id: pokemonData.id,
+                types: pokemonData.types.map(type => type.name),
+                background: pokemonData.types[0].name,
+                picture: getPokemonImage(pokemonData),
+                evolution: [],
             }))
         }
-    }, [pokemonData])
+    }, [pokemonData]);
+
+    useEffect(() => {
+        console.log('Evoluções:', evolutionData);
+        if(evolutionData) {
+            setPokemon(prevState => ({
+                ...prevState,
+                evolution: evolutionData.map(evolution => ({
+                    name: evolution.name,
+                    picture: evolution.photo,
+                    types: evolution.types,
+                }))
+            }))
+        }
+    }, [evolutionData, name]);
 
     return pokemon;
 };
