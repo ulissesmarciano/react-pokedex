c22234b2532398a10237b919de44774890736f5c
Ulisses <ulissesmkt2104@gmail.com>
2024-11-19 18:16:23 -0300

    Atualização: Criação da lista suspensa dentro do componente

--- src/components/filter-dropdown/index.jsx
+++ src/components/filter-dropdown/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Container } from './styles';
 import { FaChevronDown } from "react-icons/fa";
+import types from '../../constants/types';
 
-const FilterDropDown = () => {
+const FilterDropDown = ({ onClickItem, onClickResetItem }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
@@ -15,7 +16,14 @@ const FilterDropDown = () => {
         <span>Tipo</span>
         <FaChevronDown className={isOpen ? 'rotate' : ''} />
       </button>
-    
+      <div className={`dropdown-list ${isOpen ? 'active open-drawer' : ''}`}>
+        <button className='reset-item' onClick={onClickResetItem}>Todos</button>
+        {types.map((type, index) => (
+          <button className='item' key={index} onClick={() => onClickItem(type)}>
+            {type}
+          </button>
+        ))}
+      </div>
     </Container>
   );
 };
--- src/components/filter-dropdown/styles.js
+++ src/components/filter-dropdown/styles.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
+
   .filter-btn {
-    height: 100%;
+    height: 1.875rem;
     padding: 0 .4rem;
 
     display: flex;
@@ -22,7 +24,6 @@ export const Container = styled.div`
     color: #f1636b;
   }
 
-  /* Estilo ativo (igual ao hover) */
   .filter-btn.active {
     background-color: transparent;
     color: #f1636b;
@@ -36,5 +37,96 @@ export const Container = styled.div`
 
   .rotate {
     transform: rotate(-180deg);
+  }
+
+  .dropdown-list {
+    display: none;
+  }
+
+  .dropdown-list.active {
+    position: absolute;
+    right: 0;
+    top: 35px;
+
+    width: 11rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: .4rem;
+
+
+    padding: 1rem;
+    border: 1px solid #b44b5060;
+    background-color: #f1f1f1;
+    border-radius: 5px;
+
+    @keyframes openDropdown {
+      0% {
+        transform: scaleY(0);
+        opacity: 0;
+      }
+
+      100% {
+        transform: scaleY(1);
+        opacity: 1;
+      }
+      
+    }
+
+    transform-origin: top;
+    animation: openDropdown 0.3s ease;
+  }
+
+  .dropdown-list .item {
+    cursor: pointer;
+
+    padding: .2rem .4rem;
+    border: 1px solid #f1636b;
+    border-radius: 1rem;
+
+    text-transform: capitalize;
+    transition: 0.3s;
+
+    &:hover {
+        background-color: #f1636b;
+        color: #fff;
+    }
+  }
+
+  .dropdown-list .reset-item {
+    cursor: pointer;
+
+    background-color: #f1636b;
+    color: #fff;
+
+    padding: .2rem .4rem;
+    border: 1px solid #f1636b;
+    border-radius: 1rem;
+
+    transition: 0.3s;
+
+    &:hover {
+      background-color: #fff;
+      color: #f1636b;
+    }
+  }
+  
+  @media (min-width: 425px) {
+    .filter-btn {
+      height: 2.2rem;
+    }
+
+    .dropdown-list.active {
+      top: 40px;
+    }
+  }
+
+  @media (min-width: 650px) {
+    .filter-btn {
+      height: 2.4rem;
+    }
+
+    .dropdown-list.active {
+      top: 43px;
+    }
   }
 `;
