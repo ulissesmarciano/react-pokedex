37f0e6fc7114851b0599190ce7cc9637dbde38d0
Ulisses <ulissesmkt2104@gmail.com>
2024-11-19 18:15:29 -0300

    Criação: Nova constante de tipos do pokémon

---
+++ src/constants/types.js
@@ -0,0 +1,22 @@
+const types = [
+    'normal',
+    'water',
+    'grass',
+    'fire',
+    'fighting',
+    'psychic',
+    'ghost',
+    'dark',
+    'fairy',
+    'flying',
+    'bug',
+    'rock',
+    'ground',
+    'ice',
+    'steel',
+    'poison',
+    'electric',
+    'dragon'
+];
+
+export default types;
