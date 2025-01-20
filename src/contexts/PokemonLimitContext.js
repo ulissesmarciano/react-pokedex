229bd1a06e03408f026e2b7d9d4c847b70a9a3b2
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-20 16:58:58 -0300

    Atualização: Formatacão de documentos no diretorio contexts

--- src/contexts/PokemonLimitContext.js
+++ src/contexts/PokemonLimitContext.js
@@ -1,16 +1,16 @@
 // src/contexts/PokemonLimitContext.js
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const PokemonLimitContext = createContext();
 
 export const usePokemonLimit = () => useContext(PokemonLimitContext);
 
 export const PokemonLimitProvider = ({ children }) => {
-    const [limit, setLimit] = useState(300);
+  const [limit, setLimit] = useState(300);
 
-    return (
-        <PokemonLimitContext.Provider value={{ limit, setLimit }}>
-            {children}
-        </PokemonLimitContext.Provider>
-    );  
+  return (
+    <PokemonLimitContext.Provider value={{ limit, setLimit }}>
+      {children}
+    </PokemonLimitContext.Provider>
+  );
 };
