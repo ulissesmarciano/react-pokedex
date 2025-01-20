996484c1efe22de490ffaf6a4308527932df43ce
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-20 17:00:39 -0300

    Atualização: Formatacão de documentos no diretorio hooks

--- src/hooks/useFetchAllPokemons.js
+++ src/hooks/useFetchAllPokemons.js
@@ -1,45 +1,45 @@
-import { useState, useEffect } from 'react';
-import api from '../services/api';
+import { useState, useEffect } from "react";
+import api from "../services/api";
 
 const useFetchAllPokemons = (limit = 300) => {
-    const [pokemons, setPokemons] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const getAllPokemons = async () => {
-            try {
-                const response = await api.get(`/pokemon?limit=${limit}`);
-                const { results } = response.data;
+  useEffect(() => {
+    const getAllPokemons = async () => {
+      try {
+        const response = await api.get(`/pokemon?limit=${limit}`);
+        const { results } = response.data;
 
-                const payloadPokemons = await Promise.all(
-                    results.map(async (pokemon) => {
-                        const { id, types, sprites } = await getMoreInfo(pokemon.url);
-                        return {
-                            name: pokemon.name,
-                            id,
-                            types,
-                            sprites,
-                        };
-                    })
-                );
-                setPokemons(payloadPokemons);
-            } catch (err) {
-                setError('Erro ao carregar os Pokémons');
-                console.error(err);
-            } finally {
-                setLoading(false);
-            }
-        };
+        const payloadPokemons = await Promise.all(
+          results.map(async (pokemon) => {
+            const { id, types, sprites } = await getMoreInfo(pokemon.url);
+            return {
+              name: pokemon.name,
+              id,
+              types,
+              sprites,
+            };
+          })
+        );
+        setPokemons(payloadPokemons);
+      } catch (err) {
+        setError("Erro ao carregar os Pokémons");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        const getMoreInfo = async (url) => {
-            const response = await api.get(url);
-            const { id, types, sprites } = response.data;
-            return { id, types, sprites };
-        };
+    const getMoreInfo = async (url) => {
+      const response = await api.get(url);
+      const { id, types, sprites } = response.data;
+      return { id, types, sprites };
+    };
 
-        getAllPokemons();
-    }, [limit]);
+    getAllPokemons();
+  }, [limit]);
 
   return { pokemons, loading, error };
 };
