cef8d3c1f64e32dc6407804ea26c5b2057d2dc45
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-20 16:59:50 -0300

    Atualização: Formatacão de documentos no diretorio constants

--- src/constants/tabs.js
+++ src/constants/tabs.js
@@ -1,9 +1,9 @@
-import AboutInfoTabScreen from '../components/about-info-tab-screen'
-import AttributesInfoTabScreen from '../components/attributes-info-tab-screen'
-import StatsInfoTabScreen from '../components/StatsInfoTabScreen/StatsInfoTabScreen'
+import AboutInfoTabScreen from "../components/about-info-tab-screen";
+import AttributesInfoTabScreen from "../components/attributes-info-tab-screen";
+import StatsInfoTabScreen from "../components/StatsInfoTabScreen/StatsInfoTabScreen";
 
 export const tabs = [
-    { id: 1, label: "About", component: AboutInfoTabScreen },
-    { id: 2, label: "Attributes", component: AttributesInfoTabScreen },
-    { id: 3, label: "Stats", component: StatsInfoTabScreen },
+  { id: 1, label: "About", component: AboutInfoTabScreen },
+  { id: 2, label: "Attributes", component: AttributesInfoTabScreen },
+  { id: 3, label: "Stats", component: StatsInfoTabScreen },
 ];
