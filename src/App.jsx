a4505993adb26929a00f17165ad4e946073a79ba
Ulisses <ulissesmarcianodev@gmail.com>
2025-06-01 15:14:10 -0300

    feat: cria um documento de rotas específico da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PokemonLimitProvider } from "./contexts/PokemonLimitContext";
-import PokedexScreen from "./pages/PokedexScreen/PokedexScreen";
-import PokemonScreen from "./pages/PokemonScreen/PokemonScreen";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <PokemonLimitProvider>
-      <Router>
-        <Routes>
-          <Route path="/pokemon/:name" element={<PokemonScreen />} />
-          <Route path="/" element={<PokedexScreen />} />
-        </Routes>
-      </Router>
+      <AppRoutes />
     </PokemonLimitProvider>
   );
 }
