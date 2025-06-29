b6e9d01b25d829fa161594b3aba0becaf05ca48f
Ulisses <ulissesmarcianodev@gmail.com>
2025-06-29 19:44:03 -0300

    test: create the SearchBar test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,19 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
