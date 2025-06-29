bd8e700346d77df157b88c59249502e6630365ff
Ulisses <ulissesmarcianodev@gmail.com>
2025-06-29 17:51:21 -0300

    test: configura ambiente de testes com Vitest e Testing Library

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
-    globals: true,
     environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.ts',
   },
 });
