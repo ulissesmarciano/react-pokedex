68136dc46833c3c54bffd064c8186a8d1eb06520
Ulisses <ulissesmarcianodev@gmail.com>
2025-06-29 22:14:16 -0300

    test: create a button test

---
+++ src/components/atoms/Button/Buton.test.tsx
@@ -0,0 +1,28 @@
+import { beforeEach, describe, vi, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Button from '@/components/atoms/Button/Button';
+import userEvent from '@testing-library/user-event';
+
+describe('Button', () => {
+  const mockClick = vi.fn();
+  const name = 'Ver Mais';
+  const variantName = 'verMaisButton';
+
+  beforeEach(() =>
+    render(<Button name={name} onClick={mockClick} variant={variantName} />),
+  );
+
+  it('should render the text button', () => {
+    const button = screen.getByText('Ver Mais');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should call onClick when the button is clicked', async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole('button', { name: /ver mais/i });
+
+    await user.click(button);
+
+    expect(mockClick).toHaveBeenCalled;
+  });
+});
